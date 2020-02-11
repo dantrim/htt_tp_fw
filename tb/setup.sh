f42d7cc9f671a51d7a6bade1217f833749352bb0
Daniel Joseph Antrim <daniel.joseph.antrim@cern.ch>
2020-02-11 15:20:14 -0800

    Setup script handles python3 or python2

--- tb/setup.sh
+++ tb/setup.sh
@@ -5,33 +5,102 @@
 # It doesn't _need_ to be a shell script.
 # It should be portable to Windows, too.
 
-if [ -d env ]; then
-	source env/bin/activate
-else
+py_cmd="python"
+pip_cmd="pip"
+env_mod="virtualenv"
 
-	# Create a virtualenv in the "env" directory.
-	# Apparently, on Windows this should be "py -m" instead.
-	# https://packaging.python.org/guides/installing-using-pip-and-virtual-environments/
-	python -m virtualenv env
+function print_usage {
+    echo "------------------------------------------------------------"
+    echo "cocotb testbench for TP-FW"
+    echo ""
+    echo " Options:"
+    echo "  --py3           use python3 (default is to use python2)"
+    echo "  -h|--help       print this help message"
+    echo ""
+    echo " Example usage:"
+    echo "  $ source setup.sh --py3"
+    echo "------------------------------------------------------------"
+}
 
-	# Activate the virtualenv.
-	source env/bin/activate
+function main {
 
-	# Install dependencies into the virtualenv.
+    use_python3=0
+    while test $# -gt 0
+    do
+        case $1 in
+            -h)
+                print_usage
+                return 0
+                ;;
+            --help)
+                print_usage
+                return 0
+                ;;
+            --py3)
+                use_python3=1
+                ;;
+            *)
+                echo "Invalid argument: $1"
+                return 1
+                ;;
+        esac
+        shift
+    done
 
-    # Install cocotb
-    pip install cocotb
+    if [[ $use_python3 == 1 ]]; then
+        py_cmd="python3"
+        pip_cmd="pip3"
+        env_mod="venv"
+    fi
 
-    # If this is python2: install the enum34 package.
-    # (If it isn't, do nothing).
-    pip2 install enum34 || true
+    if [ -d env ]; then
+        source env/bin/activate
+    else
 
-    # Install the local "tptest" package.
-    pip install -e tptest
+        # Activate the virtual environtment and call it "env"
+        $py_cmd -m $env_mod env
+        source env/bin/activate
 
-    # Install the "system-simulation" DataFormat package,
-    # also in editable mode. Note: requires this to be cloned.
-    pip install -e ../../system-simulation/
-fi
+        # Install dependencies
+        $pip_cmd install cocotb
+        if [[ $use_python3 == 0 ]]; then
+            $pip_cmd install enum34
+        fi
+        $pip_cmd install -e tptest
+        $pip_cmd install -e ../../system-simulation/
+    fi
 
-echo "Activated virtual environment for cocotb. Type 'deactivate' to restore normal environment."
+    echo "Activated virtual environment for cocotb. Type 'deactivate' to restore normal environment."
+}
+
+main $*
+
+#if [ -d env ]; then
+#	source env/bin/activate
+#else
+#
+#	# Create a virtualenv in the "env" directory.
+#	# Apparently, on Windows this should be "py -m" instead.
+#	# https://packaging.python.org/guides/installing-using-pip-and-virtual-environments/
+#    python3 -m venv env
+#
+#	# Activate the virtualenv.
+#	source env/bin/activate
+#
+#	# Install dependencies into the virtualenv.
+#
+#    # Install cocotb
+#    pip3 install cocotb
+#
+#    # If this is python2: install the enum34 package.
+#    # (If it isn't, do nothing).
+#    #pip2 install enum34 || true
+#
+#    # Install the local "tptest" package.
+#    pip3 install -e tptest
+#
+#    # Install the "system-simulation" DataFormat package,
+#    # also in editable mode. Note: requires this to be cloned.
+#    pip3 install -e ../../system-simulation/
+#fi
+
