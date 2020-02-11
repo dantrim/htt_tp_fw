b41529999b9eb5b121f30696363a244bebd05a66
Daniel Joseph Antrim <daniel.joseph.antrim@cern.ch>
2020-02-11 12:20:41 -0800

    test

--- tb/setup.sh
+++ tb/setup.sh
@@ -1,21 +1,21 @@
-#!/bin/bash
+#!/bin/tcsh
 
 # Setup script for tp-fw cocotb testbench.
-# This requires: python 2.7+ with pip3 and virtualenv.
+# This requires: python 2.7+ with pip and virtualenv.
 # It doesn't _need_ to be a shell script.
 # It should be portable to Windows, too.
 
-#if [ -d env ]; then
-#	source env/bin/activate.sh
-#else
+if [ -d env ]; then
+	source env/bin/activate.sh
+else
 
 	# Create a virtualenv in the "env" directory.
 	# Apparently, on Windows this should be "py -m" instead.
-	# https://packaging.python.org/guides/installing-using-pip3-and-virtual-environments/
+	# https://packaging.python.org/guides/installing-using-pip-and-virtual-environments/
 	python -m virtualenv env
 
 	# Activate the virtualenv.
-	source env/bin/activate
+	source env/bin/activate.sh
 
 	# Install dependencies into the virtualenv.
 
@@ -24,7 +24,7 @@
 
     # If this is python2: install the enum34 package.
     # (If it isn't, do nothing).
-    pip install enum34 || true
+    pip2 install enum34 || true
 
     # Install the local "tptest" package.
     pip install -e tptest
@@ -32,6 +32,6 @@
     # Install the "system-simulation" DataFormat package,
     # also in editable mode. Note: requires this to be cloned.
     pip install -e ../../system-simulation/
-#fi
+fi
 
 echo "Activated virtual environment for cocotb. Type 'deactivate' to restore normal environment."
