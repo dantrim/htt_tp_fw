96046002ad4b3a4474b8c381af8d92e2f77b1e54
Daniel Joseph Antrim <daniel.joseph.antrim@cern.ch>
2020-05-25 09:08:49 -0700

    add check for kinit in setup.py (HACK)

--- tb/setup.py
+++ tb/setup.py
@@ -1,13 +1,19 @@
 from setuptools import setup, find_packages
 import os
+import subprocess
 
 # dantrim May 2020:
 # This is a hack but it appears to work and I coudln't get anything from
 # stackoverflow to work. It also does not help that the package is camelcased
 # (fun fact: Unix filesystem is case-insensitive!)
 # and matches in name with an existing package on PyPi...
-# system_sim_path = "https://:@gitlab.cern.ch:8443/atlas-tdaq-p2-firmware/tdaq-htt-firmware/system-simulation.git"
-system_sim_path = "https://gitlab.cern.ch/atlas-tdaq-p2-firmware/tdaq-htt-firmware/system-simulation.git"
+
+# check if kinit is there
+rc = subprocess.call(["which", "kinit"])
+if rc == 0:
+    system_sim_path = "https://:@gitlab.cern.ch:8443/atlas-tdaq-p2-firmware/tdaq-htt-firmware/system-simulation.git"
+else:
+    system_sim_path = "https://gitlab.cern.ch/atlas-tdaq-p2-firmware/tdaq-htt-firmware/system-simulation.git"
 system_sim_branch = "python3_import"
 os.system("pip install git+{}@{}".format(system_sim_path, system_sim_branch))
 
